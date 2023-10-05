d45002b126c5c5204080b5335cc1addab20d9568
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-10-05 12:06:53 +0300

    Set up firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore, serverTimestamp
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ3xx-0xxeQnPwyIe_KGFqVICsJfL_Puc",
+  authDomain: "vue-firebase-sites-86455.firebaseapp.com",
+  projectId: "vue-firebase-sites-86455",
+  storageBucket: "vue-firebase-sites-86455.appspot.com",
+  messagingSenderId: "648359853076",
+  appId: "1:648359853076:web:dc1ac1409af1547a00bb01"
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const timestamp = serverTimestamp();
+
+export { db, timestamp };
