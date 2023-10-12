33e4388d80febdd10e7797f07ed0cdadd2fd529c
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-10-12 21:38:58 +0300

    Install vue-i18n package and plugin

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const requireNoAuth = (to, from, next) => {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
