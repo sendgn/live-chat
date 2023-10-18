0d5e5da1ddcec317bfcde16912f863eab2ee7e4d
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-10-18 20:19:44 +0300

    Add language switcher component

---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+import Tr from '../i18n/translate';
+
+export default {
+  setup() {
+    const { t, locale } = useI18n();
+
+    const supportedLocales = Tr.supportedLocales;
+
+    const switchLanguage = async (event) => {
+      
+    }
+
+    return { supportedLocales };
+  }
+}
+</script>
